66358d3c268a409cf9438abac9107fc6c4fbcfdd
natsuki <natsuki.kaminaga@myrefer.co.jp>
2020-11-22 00:45:45 +0900

    ページネーションできるようにした

--- wp-content/themes/kokubunji/news-paginate.php
+++ wp-content/themes/kokubunji/news-paginate.php
@@ -1,47 +1,58 @@
-<div class="pager">
-    <?php
-    // ページャ用に現在ページを取得
-    $paged = get_query_var('paged') ? get_query_var('paged') : 1;
-    $args = array(
-        'post_type' => get_post_type(),
-        'paged' => $paged // 現在のページ数をセット
-    );
+<?php
+function pagenation( $pages, $paged, $range = 2, $show_only = false ) {
 
-    $arc_query = new WP_Query($args);
-
-    $posts = get_posts( $args );
-
-    if ( $posts ) :
-        foreach ( $posts as $post ) :
-            setup_postdata( $post );
-    ?>
-        <!-- タイトル、リンクなどの表示処理 -->
-    <?php
-            // ループ終了
-            endforeach;
-        endif;
-
-        // ページャ用設定
-        global $wp_rewrite;
-        $paginate_base = get_pagenum_link(1);
-        if(strpos($paginate_base, '?') || ! $wp_rewrite->using_permalinks()){
-            $paginate_format = '';
-            $paginate_base = add_query_arg('paged','%#%');
+    $pages = ( int ) $pages;    //float型で渡ってくるので明示的に int型 へ
+    $paged = $paged ?: 1;       //get_query_var('paged')をそのまま投げても大丈夫なように
+  
+    //表示テキスト
+    $text_first   = "«";
+    $text_before  = "«";
+    $text_next    = "»";
+    $text_last    = "»";
+  
+    if ( $show_only && $pages === 1 ) {
+        // １ページのみで表示設定が true の時
+        echo '<ul class="pager"><li class="active">1</li></ul>';
+        return;
+    }
+  
+    if ( $pages === 1 ) return;    // １ページのみで表示設定もない場合
+  
+    if ( 1 !== $pages ) {
+        //２ページ以上の時
+        echo '<ul class="pager">';
+        if ( $paged > $range + 1 ) {
+            // 「最初へ」 の表示
+            echo '<li><a href="', get_pagenum_link(1) ,'" class="first">', $text_first ,'</a></li>';
         }
-        else{
-            $paginate_format = (substr($paginate_base,-1,1) == '/' ? '' : '/') . user_trailingslashit('page/%#%/','paged');
-            $paginate_base .= '%_%';
+        if ( $paged > 1 ) {
+            // 「前へ」 の表示
+            echo '<li><a href="', get_pagenum_link( $paged - 1 ) ,'" class="prev">', $text_before ,'</a></li>';
         }
-        // ページャを出力する
-        echo paginate_links(array(
-            'base' => $paginate_base,
-            'format' => $paginate_format,
-            'total' => $arc_query->max_num_pages, // トータルのページ数をセット
-            'mid_size' => 1,
-            'current' => ($paged ? $paged : 1), // 現在のページ数をセット
-            'prev_text' => '«',
-            'next_text' => '»',
-            )
-        );
-    ?>
-</div>
+        for ( $i = 1; $i <= $pages; $i++ ) {
+  
+            if ( $i <= $paged + $range && $i >= $paged - $range ) {
+                // $paged +- $range 以内であればページ番号を出力
+                if ( $paged === $i ) {
+                    echo '<li class="active">', $i ,'</li>';
+                } else {
+                    echo '<li><a href="', get_pagenum_link( $i ) ,'">', $i ,'</a></li>';
+                }
+            }
+        }
+        if ( $paged < $pages ) {
+            // 「次へ」 の表示
+            echo '<li><a href="', get_pagenum_link( $paged + 1 ) ,'" class="next">', $text_next ,'</a></li>';
+        }
+        if ( $paged + $range < $pages ) {
+            // 「最後へ」 の表示
+            echo '<li><a href="', get_pagenum_link( $pages ) ,'" class="last">', $text_last ,'</a></li>';
+        }
+        echo '</ul>';
+    }
+  }
+
+pagenation( $wp_query->max_num_pages, get_query_var( 'paged' ), 2, true);
+
+wp_reset_postdata();
+?>
